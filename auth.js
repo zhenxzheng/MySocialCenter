9ff06e6d82906f6d0cce9c91233e8fa379ddbcb3
Zhen Zheng <zxzheng@ucsd.edu>
2014-04-20 18:56:45 -0700

    deleted comments

--- auth.js
+++ auth.js
@@ -21,17 +21,3 @@ var tconf = {
 exports.graph = graph;
 exports.conf = 	conf;
 exports.tconf = tconf;
-
-
-
-
-
-/**
-//add instagram api setup
-var ig = require('instagram-node-lib');
-ig.set('client_id', process.env.instagram_client_id);
-ig.set('client_secret', process.env.instagram_client_secret);
-
-//export ig as a parameter to be used by other methods that require it.
-exports.ig = ig;
-**/
